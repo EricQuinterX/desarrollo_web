c7a61e36e090297347ba4e433a9d0d421f3cf65a
Eric Brian Quinteros <Eric Brian Quinteros>
2017-01-15 23:58:14 +0000

    React y webpack

---
+++ 009_react-02/src/js/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="container-fluid">
+        <h4>Eric Tutorial React 2017</h4>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ 009_react-02/src/js/instances/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import Footer from '../Components/Footer';
+import App from '../Components/App';
+import '../../css/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <Footer />,
+  document.getElementById('pie')
+);
