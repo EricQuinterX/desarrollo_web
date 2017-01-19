67b05573f39924e1b6e9615c273b5611b029a499
Eric Brian Quinteros <Eric Brian Quinteros>
2017-01-19 00:23:31 +0000

    avance react con webpack

--- 009_react-02/src/app/index.js
+++ 009_react-02/src/app/index.js
@@ -1,1 +1,14 @@
-console.log("it works!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Hola Mundo!!!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
